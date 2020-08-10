aa3cb128267fdb1fd29348f8a2002ade99f6a0aa
Krishna Golla <raghavender7@gmail.com>
2020-08-10 01:05:46 -0400

    Update deploy.sh

--- pattern-datafactory-databricks/components/data-factory/deploy.sh
+++ pattern-datafactory-databricks/components/data-factory/deploy.sh
@@ -6,5 +6,5 @@ resourceGroupLocation="eastus2"
 # Create RGs
 az group create --name $eastADFResourceGroupName --location $resourceGroupLocation
 
-# Create Dara Factory
+# Create Data Factory
 az deployment group create --resource-group $eastADFResourceGroupName --name ADFdeployment  --template-file adfazuredeploy.json --parameters name="adfsrgoeastus4"
