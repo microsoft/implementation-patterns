96836338b6e967bd7337e4428782ec1aead4e667
Krishna Golla <raghavender7@gmail.com>
2020-08-09 20:41:35 -0400

    Update deploy.sh

--- pattern-datafactory-databricks/components/base-network/deploy.sh
+++ pattern-datafactory-databricks/components/base-network/deploy.sh
@@ -11,5 +11,5 @@ az group create --name $eastNetworkResourceGroupName --location $resourceGroup1L
 az group create --name $centralNetworkResourceGroupName --location $resourceGroup2Location
 
 # Create VNets
-az deployment group create --resource-group $eastNetworkResourceGroupName --name network-eastus2 --template-file azuredeploy.json --parameters hubVnetPrefix="10.0.0.0/16" firewallSubnetPrefix="10.0.1.0/24" DNSSubnetPrefix="10.0.2.0/24" spokeVnetPrefix="10.1.0.0/16" workloadSubnetPrefix="10.1.2.0/24"
-az deployment group create --resource-group $centralNetworkResourceGroupName --name network-centralus --template-file azuredeploy.json --parameters hubVnetPrefix="10.2.0.0/16" firewallSubnetPrefix="10.2.1.0/24" DNSSubnetPrefix="10.2.2.0/24" spokeVnetPrefix="10.3.0.0/16" workloadSubnetPrefix="10.3.2.0/24"
+az deployment group create --resource-group $eastNetworkResourceGroupName --name network-eastus2 --template-file azuredeploy-vnet.json --parameters hubVnetPrefix="10.0.0.0/16" firewallSubnetPrefix="10.0.1.0/24" DNSSubnetPrefix="10.0.2.0/24" spokeVnetPrefix="10.1.0.0/16" workloadSubnetPrefix="10.1.2.0/24"
+az deployment group create --resource-group $centralNetworkResourceGroupName --name network-centralus --template-file azuredeploy-vnet.json --parameters hubVnetPrefix="10.2.0.0/16" firewallSubnetPrefix="10.2.1.0/24" DNSSubnetPrefix="10.2.2.0/24" spokeVnetPrefix="10.3.0.0/16" workloadSubnetPrefix="10.3.2.0/24"
