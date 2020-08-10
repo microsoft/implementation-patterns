f779b967568215b04787cb85f7dfa49856e0581c
Krishna Golla <raghavender7@gmail.com>
2020-08-10 01:01:38 -0400

    Update deploy.sh

--- pattern-datafactory-databricks/components/data-factory/deploy.sh
+++ pattern-datafactory-databricks/components/data-factory/deploy.sh
@@ -4,5 +4,8 @@
 name="adfsrgoeastus2"
 resourceGroupLocation="eastus2"
 
+# Create RGs
+az group create --name $eastADFResourceGroupName --location $resourceGroupLocation
+
 # Create VNets
-az deployment group create --resource-group $resourceGroupLocation --name ADFdeployment  --template-file adfazuredeploy.json --parameters name="adfsrgoeastus3"
+az deployment group create --resource-group $eastADFResourceGroupName --name ADFdeployment  --template-file adfazuredeploy.json --parameters name="adfsrgoeastus4"
