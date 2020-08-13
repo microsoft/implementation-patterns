c3e8925bd2bd8beb805c5a4b3d2ed63353089783
ken <kskvarci@gmail.com>
2020-08-13 13:30:23 -0400

    app

--- pattern-functions-servicebus/components/functions/deploy.sh
+++ pattern-functions-servicebus/components/functions/deploy.sh
@@ -10,6 +10,9 @@ centralNetworkResourceGroupName="Network-RG-CentralUS"
 eastWorkloadResourceGroupName="Workload-RG-EastUS2"
 centralWorkloadResourceGroupName="Workload-RG-CentralUS"
 
+eastAppName="demappeast1"
+centralAppName="demappcentral1"
+
 eastPlanName="demplaneast1"
 centralPlanName="demplancentral1"
 
@@ -33,3 +36,6 @@ az deployment group create --resource-group $centralWorkloadResourceGroupName --
 az deployment group create --resource-group $eastWorkloadResourceGroupName --name storage-eastus2 --template-file azuredeploy-storage.json --parameters storageAccountName=$eastStorageName applicationId=$appId employeeId=$employeeID organization=$org environment=$env
 az deployment group create --resource-group $centralWorkloadResourceGroupName --name storage-centralus --template-file azuredeploy-storage.json --parameters storageAccountName=$centralStorageName applicationId=$appId employeeId=$employeeID organization=$org environment=$env
 
+# Create Function Apps
+az deployment group create --resource-group $eastWorkloadResourceGroupName --name app-eastus2 --template-file azuredeploy-app.json --parameters appName=$eastAppName planName=$eastPlanName storageAccountName=$eastStorageName applicationId=$appId employeeId=$employeeID organization=$org environment=$env
+az deployment group create --resource-group $centralWorkloadResourceGroupName --name app-centralus --template-file azuredeploy-app.json --parameters appName=$centralAppName planName=$eastPlanName storageAccountName=$centralStorageName applicationId=$appId employeeId=$employeeID organization=$org environment=$env
