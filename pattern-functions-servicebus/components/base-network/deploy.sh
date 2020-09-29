31163bf5e68954b6d50bf430f418b0743eaf54a1
Patrick El-Azem <paelaz@microsoft.com>
2020-09-29 07:54:58 -0400

    Refactor log analytics out of Service Bus base network pattern, since now have separate standalone Diagnostics folder

--- pattern-functions-servicebus/components/base-network/deploy.sh
+++ pattern-functions-servicebus/components/base-network/deploy.sh
@@ -11,23 +11,10 @@ location2="centralus"
 resourceGroup1NameNet="$infix-net-$location1"
 resourceGroup2NameNet="$infix-net-$location2"
 
-# Log Analytics RG - can be per region or only one
-logAnalyticsResourceGroupName1="sbtest" # Leave blank if you don't want LA diagnostics. Should already exist - not created here
-logAnalyticsResourceGroupName2="sbtest" # Leave blank if you don't want LA diagnostics. Should already exist - not created here
-# Log Analytics Workspace - for NSG Flow Logging, should be in same region as NSG
-logAnalyticsWorkspaceName1="sbp-la-""$location1" # Workspace should already exist - not created here
-logAnalyticsWorkspaceName2="sbp-la-""$location2" # Workspace should already exist - not created here
-# Storage Account used by NSG Flow Logging - should be in same region as NSG/LA Workspace
-flowLogStorageAccountName1="sbopssa""$location1" #Storage account should already exist - not created here
-flowLogStorageAccountName2="sbopssa""$location2" #Storage account should already exist - not created here
-
 templateFileVnet="azuredeploy-vnet.json"
 
 
 # Create first set of network resources
-
-echo "Create first set of network resources"
-
 hubVnetName="hub-vnet"
 hubVnetPrefix="10.0.0.0/16"
 hubVnetFirewallSubnetName="AzureFirewallSubnet"
@@ -51,97 +38,9 @@ az deployment group create --subscription "$subscription_id" --resource-group "$
 	workloadSubnetName="$spokeVnetWorkloadSubnetName" workloadSubnetPrefix="$spokeVnetWorkloadSubnetPrefix" \
 	workloadNsgName="$spokeVnetWorkloadNSGName"
 
-if [ $logAnalyticsResourceGroupName1 ]
-then
-	echo "Create Diagnostics Settings for Network Resources 1"
-
-	# Get Resource IDs for Diagnostics
-	hubVnetResourceId="$(az network vnet show --subscription "$subscription_id" --resource-group "$resourceGroup1NameNet" --name "$hubVnetName"  -o tsv --query "id")"
-	spokeVnetResourceId="$(az network vnet show --subscription "$subscription_id" --resource-group "$resourceGroup1NameNet" --name "$spokeVnetName"  -o tsv --query "id")"
-	workloadNsgResourceId="$(az network nsg show --subscription "$subscription_id" --resource-group "$resourceGroup1NameNet" --name "$spokeVnetWorkloadNSGName"  -o tsv --query "id")"
-
-	# Configure Log Analytics Diagnostics for hub VNet
-	# Get categories using https://docs.microsoft.com/cli/azure/monitor/diagnostic-settings/categories?view=azure-cli-latest#az_monitor_diagnostic_settings_categories_list
-	az monitor diagnostic-settings create --subscription "$subscription_id" --name "$hubVnetName""-diag" --verbose \
-		--resource "$hubVnetResourceId" --resource-group "$logAnalyticsResourceGroupName1" --workspace "$logAnalyticsWorkspaceName1" \
-		--logs '[
-			{
-				"category": "VMProtectionAlerts",
-				"enabled": true,
-				"retentionPolicy": {
-					"enabled": false,
-					"days": 0
-				}
-			}
-		]' \
-		--metrics '[
-			{
-				"category": "AllMetrics",
-				"enabled": true,
-				"retentionPolicy": {
-					"enabled": false,
-					"days": 0
-				}
-			}
-		]'
-
-	# Configure Log Analytics Diagnostics for spoke VNet
-	az monitor diagnostic-settings create --subscription "$subscription_id" --name "$spokeVnetName""-diag" --verbose \
-		--resource "$spokeVnetResourceId" --resource-group "$logAnalyticsResourceGroupName1" --workspace "$logAnalyticsWorkspaceName1" \
-		--logs '[
-			{
-				"category": "VMProtectionAlerts",
-				"enabled": true,
-				"retentionPolicy": {
-					"enabled": false,
-					"days": 0
-				}
-			}
-		]' \
-		--metrics '[
-			{
-				"category": "AllMetrics",
-				"enabled": true,
-				"retentionPolicy": {
-					"enabled": false,
-					"days": 0
-				}
-			}
-		]'
-
-	# Configure Log Analytics Diagnostics for workload NSG
-	az monitor diagnostic-settings create --subscription "$subscription_id" --name "$spokeVnetWorkloadNSGName""-diag" --verbose \
-		--resource "$workloadNsgResourceId" --resource-group "$logAnalyticsResourceGroupName1" --workspace "$logAnalyticsWorkspaceName1" \
-		--logs '[
-			{
-				"category": "NetworkSecurityGroupEvent",
-				"enabled": true,
-				"retentionPolicy": {
-					"enabled": false,
-					"days": 0
-				}
-			},
-			{
-				"category": "NetworkSecurityGroupRuleCounter",
-				"enabled": true,
-				"retentionPolicy": {
-					"enabled": false,
-					"days": 0
-				}
-			}
-		]'
-
-	# Configure NSG flow logging - note: limit of 1 per subscription and region. If you already have NSG flow logger in sub/region, then use az network watcher flow-log configure instead.
-	az network watcher flow-log create --subscription "$subscription_id" --location "$location1" --name "$spokeVnetWorkloadNSGName""-flowlog" --verbose \
-		--enabled true --traffic-analytics true --nsg "$workloadNsgResourceId" --interval 10 --log-version 2 --retention 0 \
-		--storage-account "$flowLogStorageAccountName1" --resource-group "$logAnalyticsResourceGroupName1" --workspace "$logAnalyticsWorkspaceName1"
-fi
-
 
 
 # Create second set of network resources
-echo "Create second set of network resources"
-
 hubVnetName="hub-vnet"
 hubVnetPrefix="10.2.0.0/16"
 hubVnetFirewallSubnetName="AzureFirewallSubnet"
@@ -164,89 +63,3 @@ az deployment group create --subscription "$subscription_id" --resource-group "$
 	spokeVnetName="$spokeVnetName" spokeVnetPrefix="$spokeVnetPrefix" \
 	workloadSubnetName="$spokeVnetWorkloadSubnetName" workloadSubnetPrefix="$spokeVnetWorkloadSubnetPrefix" \
 	workloadNsgName="$spokeVnetWorkloadNSGName"
-
-if [ $logAnalyticsResourceGroupName2 ]
-then
-	echo "Create Diagnostics Settings for Network Resources 2"
-
-	# Get Resource IDs for Diagnostics
-	hubVnetResourceId="$(az network vnet show --subscription "$subscription_id" --resource-group "$resourceGroup2NameNet" --name "$hubVnetName"  -o tsv --query "id")"
-	spokeVnetResourceId="$(az network vnet show --subscription "$subscription_id" --resource-group "$resourceGroup2NameNet" --name "$spokeVnetName"  -o tsv --query "id")"
-	workloadNsgResourceId="$(az network nsg show --subscription "$subscription_id" --resource-group "$resourceGroup2NameNet" --name "$spokeVnetWorkloadNSGName"  -o tsv --query "id")"
-
-	# Configure Log Analytics Diagnostics for hub VNet
-	# Get categories using https://docs.microsoft.com/cli/azure/monitor/diagnostic-settings/categories?view=azure-cli-latest#az_monitor_diagnostic_settings_categories_list
-	az monitor diagnostic-settings create --subscription "$subscription_id" --name "$hubVnetName""-diag" --verbose \
-		--resource "$hubVnetResourceId" --resource-group "$logAnalyticsResourceGroupName2" --workspace "$logAnalyticsWorkspaceName2" \
-		--logs '[
-			{
-				"category": "VMProtectionAlerts",
-				"enabled": true,
-				"retentionPolicy": {
-					"enabled": false,
-					"days": 0
-				}
-			}
-		]' \
-		--metrics '[
-			{
-				"category": "AllMetrics",
-				"enabled": true,
-				"retentionPolicy": {
-					"enabled": false,
-					"days": 0
-				}
-			}
-		]'
-
-	# Configure Log Analytics Diagnostics for spoke VNet
-	az monitor diagnostic-settings create --subscription "$subscription_id" --name "$spokeVnetName""-diag" --verbose \
-		--resource "$spokeVnetResourceId" --resource-group "$logAnalyticsResourceGroupName2" --workspace "$logAnalyticsWorkspaceName2" \
-		--logs '[
-			{
-				"category": "VMProtectionAlerts",
-				"enabled": true,
-				"retentionPolicy": {
-					"enabled": false,
-					"days": 0
-				}
-			}
-		]' \
-		--metrics '[
-			{
-				"category": "AllMetrics",
-				"enabled": true,
-				"retentionPolicy": {
-					"enabled": false,
-					"days": 0
-				}
-			}
-		]'
-
-	# Configure Log Analytics Diagnostics for workload NSG
-	az monitor diagnostic-settings create --subscription "$subscription_id" --name "$spokeVnetWorkloadNSGName""-diag" --verbose \
-		--resource "$workloadNsgResourceId" --resource-group "$logAnalyticsResourceGroupName2" --workspace "$logAnalyticsWorkspaceName2" \
-		--logs '[
-			{
-				"category": "NetworkSecurityGroupEvent",
-				"enabled": true,
-				"retentionPolicy": {
-					"enabled": false,
-					"days": 0
-				}
-			},
-			{
-				"category": "NetworkSecurityGroupRuleCounter",
-				"enabled": true,
-				"retentionPolicy": {
-					"enabled": false,
-					"days": 0
-				}
-			}
-		]'
-
-	# Configure NSG flow logging - note: limit of 1 per subscription and region. If you already have NSG flow logger in sub/region, then use az network watcher flow-log configure instead.
-	az network watcher flow-log create --subscription "$subscription_id" --location "$location2" --name "$spokeVnetWorkloadNSGName""-flowlog" --verbose \
-		--enabled true --traffic-analytics true --nsg "$workloadNsgResourceId" --interval 10 --log-version 2 --retention 0 \
-		--storage-account "$flowLogStorageAccountName2" --resource-group "$logAnalyticsResourceGroupName2" --workspace "$logAnalyticsWorkspaceName2"
-fi
