71b103bb4df9cda08647dd14326006be8fb11d60
Patrick El-Azem <paelaz@microsoft.com>
2020-10-23 17:55:22 -0400

    Add sh to enable subscription activity log to be sent to a Log Analytics workspace

--- diagnostics-log-analytics/components/query.diag.sh
+++ diagnostics-log-analytics/components/query.diag.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Params
-subscription_id="e61e4c75-268b-4c94-ad48-237aa3231481"
-location="eastus"
-log_analytics_resource_group_name="sbtest"
-log_analytics_workspace_name="pz-la-eastus2"
-resource_id="/SUBSCRIPTIONS/E61E4C75-268B-4C94-AD48-237AA3231481/RESOURCEGROUPS/SBTEST/PROVIDERS/MICROSOFT.SERVICEBUS/NAMESPACES/PZ-SB-EASTUS2"
+subscription_id=""
+location=""
+log_analytics_resource_group_name=""
+log_analytics_workspace_name=""
+resource_id=""
 
 # Variables
 workspace_resource_id="$(az monitor log-analytics workspace show --subscription "$subscription_id" -g "$log_analytics_resource_group_name" -n "$log_analytics_workspace_name" -o tsv --query 'id')"
