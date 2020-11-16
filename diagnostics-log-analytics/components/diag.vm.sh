09c6ae5e2c78afb8fb15fa6609c3a3216fb761e8
Patrick El-Azem <paelaz@microsoft.com>
2020-11-16 16:11:29 -0500

    Add/update diagnostics scripts: configure all public IPs in a RG, iterate VM config

--- diagnostics-log-analytics/components/diag.vm.sh
+++ diagnostics-log-analytics/components/diag.vm.sh
@@ -34,15 +34,24 @@ az monitor diagnostic-settings show --name "$resource_name""-diag" --resource "$
 
 if [ $storage_acct_name ]
 then
+	# Get storage account connection string
+	storage_acct_connection_string="$(az storage account show-connection-string --subscription "$subscription_id" -g "$resource_group_name" -n "$storage_acct_name" -o tsv)"
+
 	# Configure Boot Diagnostics
+	echo "Configure Boot Diagnostics"
 	az vm boot-diagnostics enable --verbose --ids "$resource_id" --storage "https://""$storage_acct_name"".blob.core.windows.net"
+
+	echo "Get default config"
+	default_config="$(az vm diagnostics get-default-config | sed "s#__DIAGNOSTIC_STORAGE_ACCOUNT__#""$storage_acct_name""#g" | sed "s#__VM_OR_VMSS_RESOURCE_ID__#""$resource_id""#g")"
+
+	echo "Get SAS token"
+	storage_sastoken="$(az storage account generate-sas --subscription ""$subscription_id"" --connection-string ""$storage_acct_connection_string"" --expiry 2037-12-31T23:59:00Z --permissions wlacu --resource-types co --services bt -o tsv)"
+
+	echo "Get protected settings"
+	protected_settings="{'storageAccountName': '""$storage_acct_name""', 'storageAccountSasToken': '""$storage_sastoken""'}"
 
 	# Configure VM diagnostics - see https://docs.microsoft.com/cli/azure/vm/diagnostics?view=azure-cli-latest#az_vm_diagnostics_set
 	# For troubleshooting, see https://aka.ms/VMExtensionLinuxDiagnosticsTroubleshoot (pay attention to all)
-
-	default_config="$(az vm diagnostics get-default-config | sed "s#__DIAGNOSTIC_STORAGE_ACCOUNT__#""$storage_acct_name""#g" | sed "s#__VM_OR_VMSS_RESOURCE_ID__#""$resource_id""#g")"
-	storage_sastoken="$(az storage account generate-sas --account-name ""$storage_acct_name"" --expiry 2037-12-31T23:59:00Z --permissions wlacu --resource-types co --services bt -o tsv)"
-	protected_settings="{'storageAccountName': '""$storage_acct_name""', 'storageAccountSasToken': '""$storage_sastoken""'}"
 
 	az vm diagnostics set --verbose --ids "$resource_id" --no-auto-upgrade $false --settings "$default_config" --protected-settings "$protected_settings"
 fi
